5ff856492639fc10b232d4bffd5661d19d2b14de
Kévin Morpain <kevin@liberkeys.com>
2020-01-02 12:17:06 +0100

    [Server] fix character games property

--- server/src/resolvers/Character.js
+++ server/src/resolvers/Character.js
@@ -3,7 +3,7 @@ function player(parent, args, context) {
 }
 
 function games(parent, args, context) {
-  return context.prisma.characters({id: parent.id }).games()
+  return context.prisma.character({id: parent.id }).games()
 }
 
 module.exports = {
